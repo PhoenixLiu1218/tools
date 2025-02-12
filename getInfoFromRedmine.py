50214aa2cb5a468e1a15f2f7161e749a7edcc11e
M.A <myk.amamiya@gmail.com>
2025-02-12 18:42:48 +0900

    last time

--- getInfoFromRedmine.py
+++ getInfoFromRedmine.py
@@ -20,7 +20,7 @@ import json
 # redmine_url = "http://172.16.58.128"
 # api_key = "4b9644679baed77016eb3735c0baf7dd96f4b224"
 
-redmine_url = "http://172.28.4.200/"
+redmine_url = "http://172.28.4.200/redmine"
 api_key = "549762c7d415b82b5ce3a28e82973b66a4da2e8a"
 
 server = Redmine(redmine_url,key=api_key)
@@ -39,38 +39,20 @@ lastUpdatedTime = ""
 #region Functions
 
 
-
 #endregion
+
+#region Main
 
 for issue in project:
     print(f"ID: {issue.id}")
     print(f"Subject: {issue.subject}")
-    print(f"Description: {issue.description}")
+    # print(f"Description: {issue.description}")
     print(f"Status: {issue.status}")
-    print(f"Priority: {issue.priority}")
+    # print(f"Priority: {issue.priority}")
     print(f"Author: {issue.author}")
-#    print(f"Assigned to: {issue.assigned_to}")
+    # print(f"Assigned to: {issue.assigned_to}")
     print(f"Created on: {issue.created_on}")
     print(f"Updated on: {issue.updated_on}")
     print("-" * 40)
 
-
-
-# url = (f"{redmine_url}/projects/{project_id}/issues.json?key={api_key}"
-#        f"&updated_on=><{start_date}|{end_date}")
-
-
-
-# end_date = datetime.now().date()
-# start_date = end_date - timedelta(days=30)
-
-
-# response = requests.get(url)
-
-# if response.status_code == 200:
-#     issues = response.json()
-
-#     formatted_issues = json.dumps(issues, indent=4, ensure_ascii=False)
-#     print(formatted_issues)
-# else:
-#     print("Error:", response.status_code, response.text)
+#endregion
